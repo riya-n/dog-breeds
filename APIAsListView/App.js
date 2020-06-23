95257301f7a10b0526b0c9ce67742338f38ad862
Riya Narayan <riya.narayan@gmail.com>
2020-06-23 21:52:16 +0800

    change to sectionlist

--- APIAsListView/App.js
+++ APIAsListView/App.js
@@ -14,6 +14,7 @@ import {
   Text,
   FlatList,
   ListView,
+  SectionList,
   ActivityIndicator,
 } from 'react-native';
 
@@ -26,14 +27,28 @@ const App: () => React$Node = () => {
       .then(response => response.json())
       .then(data => {
         let dogs = [];
+        let currDogs = [];
+        let curr = 'A';
         Object.keys(data.message).forEach(breed => {
-          const dog = {
-            key: breed,
-            name: breed.charAt(0).toUpperCase() + breed.slice(1),
-            types: data.message[breed],
-          };
-          dogs.push(dog);
+          const name = breed.charAt(0).toUpperCase() + breed.slice(1);
+          if (name.charAt(0) === curr) {
+            currDogs.push(name);
+          } else {
+            const section = {
+              heading: curr,
+              data: currDogs,
+            };
+            dogs.push(section);
+            curr = name.charAt(0);
+            currDogs = [];
+            currDogs.push(name);
+          }
         });
+        const section = {
+          heading: curr,
+          data: currDogs,
+        };
+        dogs.push(section);
         setData(dogs);
       })
       .catch(error => console.error(error))
@@ -42,19 +57,23 @@ const App: () => React$Node = () => {
 
   return (
     <>
-      <SafeAreaView>
+      <SafeAreaView style={{marginBottom: 130}}>
+        <Text style={styles.title}>List of Dog Breeds</Text>
         <View style={styles.container}>
           {isLoading ? (
             <ActivityIndicator />
           ) : (
-            <FlatList
-              data={data}
-              keyExtractor={(item, i) => i.toString()}
+            <SectionList
+              sections={data}
               renderItem={({item}, i) => (
                 <View id={i} style={styles.container}>
-                  <Text style={styles.name}>{item.name}</Text>
+                  <Text style={styles.name}>{item}</Text>
                 </View>
               )}
+              renderSectionHeader={({section}) => (
+                <Text style={styles.heading}>{section.heading}</Text>
+              )}
+              keyExtractor={(item, i) => i.toString()}
             />
           )}
         </View>
@@ -69,13 +88,24 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     flexDirection: 'row',
   },
-  initial: {
-    paddingRight: 10,
+  title: {
+    textAlign: 'center',
+    paddingTop: 30,
     fontSize: 20,
     fontWeight: 'bold',
   },
   name: {
-    fontSize: 20,
+    fontSize: 18,
+    height: 30,
+  },
+  heading: {
+    paddingTop: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    backgroundColor: 'gainsboro',
   },
 });
 
